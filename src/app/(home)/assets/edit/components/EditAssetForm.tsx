ee19923320c241fe08aa5bd3b3fdebea171e4805
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2025-10-28 10:49:30 +0000

    eslint edit errors

--- src/app/(home)/assets/edit/components/EditAssetForm.tsx
+++ src/app/(home)/assets/edit/components/EditAssetForm.tsx
@@ -21,9 +21,65 @@ import Input from "@/components/input/Input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Icon } from "@/libs";
-import { Asset, Sites, User } from "@/types";
+import { Asset, Sites } from "@/types";
 import { editAssetService } from "@/app/actions";
 import dayjs from "dayjs";
+
+interface USER_TYPE {
+  country: string;
+  created_at: number;
+  created_at_datetime: string;
+  date_of_birth: string;
+  email: string;
+  first_name: string;
+  id: string;
+  last_name: string;
+  organization: {
+    created_at: number;
+    created_at_datetime: string;
+    description: string;
+    id: string;
+    industry: string;
+    logo_url: string;
+    members: unknown;
+    name: string;
+    owner: unknown;
+    sites: unknown;
+    team_strength: string;
+    updated_at: number;
+    updated_at_datetime: string;
+  };
+  password_hash: string;
+  phone: string;
+  role: string;
+  role_id: string | null;
+  role_obj: unknown;
+  site: {
+    address: string;
+    attachments: null;
+    city: string;
+    country: string;
+    created_at: number;
+    created_at_datetime: string;
+    description: string;
+    id: string;
+    installation_environment: string;
+    manager_email: string;
+    manager_location: string;
+    manager_name: string;
+    manager_phone_number: string;
+    members: unknown;
+    name: string;
+    organization: unknown;
+    regulations_and_standards: unknown;
+    tag: string;
+    updated_at: number;
+    updated_at_datetime: string;
+  };
+  status: string;
+  updated_at: number;
+  updated_at_datetime: string;
+}
 
 type FormData = z.infer<typeof ADD_ASSET_SCHEMA>;
 
@@ -34,7 +90,7 @@ export const EditAssetForm = ({
   asset,
 }: {
   sites: Sites[];
-  users: User[];
+  users: USER_TYPE[];
   assetId?: string;
   asset?: Asset;
 }) => {
