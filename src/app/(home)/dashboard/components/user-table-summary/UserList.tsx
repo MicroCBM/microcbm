d7fb148e3cefc2ea1ebc6887075f6bd3ff28a2d2
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2025-10-28 10:58:15 +0000

    user type

--- src/app/(home)/dashboard/components/user-table-summary/UserList.tsx
+++ src/app/(home)/dashboard/components/user-table-summary/UserList.tsx
@@ -1,6 +1,62 @@
 import { UserTable } from "@/app/(home)/user-management/components";
-import { Organization, Sites, User } from "@/types";
+import { Organization, Sites } from "@/types";
 import React from "react";
+
+interface USER_TYPE {
+  country: string;
+  created_at: number;
+  created_at_datetime: string;
+  date_of_birth: string;
+  email: string;
+  first_name: string;
+  id: string;
+  last_name: string;
+  organization: {
+    created_at: number;
+    created_at_datetime: string;
+    description: string;
+    id: string;
+    industry: string;
+    logo_url: string;
+    members: unknown;
+    name: string;
+    owner: unknown;
+    sites: unknown;
+    team_strength: string;
+    updated_at: number;
+    updated_at_datetime: string;
+  };
+  password_hash: string;
+  phone: string;
+  role: string;
+  role_id: string | null;
+  role_obj: unknown;
+  site: {
+    address: string;
+    attachments: null;
+    city: string;
+    country: string;
+    created_at: number;
+    created_at_datetime: string;
+    description: string;
+    id: string;
+    installation_environment: string;
+    manager_email: string;
+    manager_location: string;
+    manager_name: string;
+    manager_phone_number: string;
+    members: unknown;
+    name: string;
+    organization: unknown;
+    regulations_and_standards: unknown;
+    tag: string;
+    updated_at: number;
+    updated_at_datetime: string;
+  };
+  status: string;
+  updated_at: number;
+  updated_at_datetime: string;
+}
 
 interface Permission {
   id: string;
@@ -26,7 +82,7 @@ export default function userList({
   organizations,
   sites,
 }: {
-  users: User[];
+  users: USER_TYPE[];
   roles: Role[];
   organizations: Organization[];
   sites: Sites[];
